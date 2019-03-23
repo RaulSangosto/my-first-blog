d3d9367cd3046b39e5a43dc0f4796f1ae33a8926
Raul <raulsangosto@gmail.com>
2019-03-23 12:30:43 +0100

    Agregadas vistas y plantillas para el detalle del post del blog asi como tambien css para el sitio.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_files'),
+                   )
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
