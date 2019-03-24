6351ebb5d9821e3da32fccf7090579e380c03c7a
Raul <raulsangosto@gmail.com>
2019-03-24 10:33:09 +0100

    Añadir posibilidad de dejar comentarios en la web.

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(
+    created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
@@ -16,6 +17,23 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
